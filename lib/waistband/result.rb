a92bef52e813186e5211bb80d0a69dcf057dfd48
David Jairala <davidjairala@gmail.com>
2014-03-12 11:15:30 -0700

    don't blow up when result doesn't have the method

--- lib/waistband/result.rb
+++ lib/waistband/result.rb
@@ -8,7 +8,7 @@ module Waistband
     def method_missing(method_name, *args, &block)
       return @result_hash[method_name.to_s] if @result_hash.keys.include?(method_name.to_s)
       return @result_hash['_source'][method_name.to_s] if @result_hash['_source'] && @result_hash['_source'].keys.include?(method_name.to_s)
-      super
+      nil
     end
 
     def respond_to_missing?(method_name, include_private = false)
