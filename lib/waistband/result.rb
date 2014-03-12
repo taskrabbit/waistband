c6ca4c1f73186cc36796d52ec0027f9c2c1bb1c3
David Jairala <davidjairala@gmail.com>
2014-03-12 11:19:14 -0700

    better respond_to_missing in result

--- lib/waistband/result.rb
+++ lib/waistband/result.rb
@@ -13,6 +13,7 @@ module Waistband
 
     def respond_to_missing?(method_name, include_private = false)
       return true if @result_hash.keys.include?(method_name.to_s)
+      return true if @result_hash['_source'] && @result_hash['_source'].keys.include?(method_name.to_s)
       super
     end
 
