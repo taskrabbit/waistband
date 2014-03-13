29672c333217c2bba5c155e4fe4acee270fd4cab
David Jairala <davidjairala@gmail.com>
2014-03-13 14:01:16 -0700

    include active_support inflections

--- lib/waistband/index.rb
+++ lib/waistband/index.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/hash/keys'
 require 'active_support/core_ext/array/extract_options'
+require 'active_support/core_ext/string/inflections'
 require 'elasticsearch'
 
 module Waistband
--- lib/waistband/version.rb
+++ lib/waistband/version.rb
@@ -1,3 +1,3 @@
 module Waistband
-  VERSION = "0.9.1"
+  VERSION = "0.9.2"
 end
