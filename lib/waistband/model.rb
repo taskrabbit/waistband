6d385977eae2d1965259b54fb589c4f86959df9e
David Jairala <davidjairala@gmail.com>
2015-02-21 21:50:23 -0800

    basic model attributes and defaults working

---
+++ lib/waistband/model.rb
@@ -0,0 +1,86 @@
+require 'active_support/core_ext/object/blank'
+
+module Waistband
+  class Model
+
+
+    #################
+    # Class methods #
+    #################
+
+    class << self
+
+      attr_reader :type_name, :column_defaults
+
+      def index_name(name)
+        @index_name = name.to_s
+      end
+
+      def index_type(name)
+        @type_name = name.to_s
+      end
+
+      def index
+        raise "index_name not defined!" if @index_name.blank?
+        Waistband::Index.new(@index_name)
+      end
+
+      def defaults(defaults)
+        @column_defaults ||= {}
+        @column_defaults.merge!(defaults)
+      end
+
+    end
+
+
+    delegate :index, :type_name, :column_defaults, to: :'self.class'
+    
+    attr_accessor :attributes
+
+
+    def initialize(attributes = {})
+      self.attributes = (column_defaults || {}).merge(attributes.symbolize_keys)
+    end
+
+
+    #########
+    # Index #
+    #########
+
+    def property_names
+      index.config['mappings'][type_name]['properties'].keys
+    end
+
+    ##############
+    # Attributes #
+    ##############
+
+    def method_missing(method_name, *args, &block)
+      method_name_without_eq = method_name.to_s.gsub('=', '')
+
+      if self.attributes.has_key?(method_name)
+        # attribute in place not by mappings but by defaults
+        return read_attribute(method_name)
+      elsif property_names.include?(method_name.to_s)
+        # attribute reader (model.attribute_name)
+        return read_attribute(method_name)
+      elsif property_names.include?(method_name_without_eq)
+        # attribute writer (model.attribute_name=)
+        write_attribute(method_name_without_eq, *args)
+      else
+        super
+      end
+    end
+
+    def read_attribute(key)
+      self.attributes[key]
+    end
+
+    def write_attribute(key, *args)
+      val = args.first
+      self.attributes[key.to_sym] = val
+    end
+
+
+  end
+end
---
+++ spec/lib/model_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Waistband::Model do
+
+  class TestModel < Waistband::Model
+
+    index_name :geo
+    index_type :geo
+
+    defaults important: true, shenanigans: 1
+
+  end
+
+  context "index" do
+
+    it "allows determining the index and type the model belongs to" do
+      expect(TestModel.index).to be_a(Waistband::Index)
+      expect(TestModel.index.send(:config_name)).to eql 'geo_test'
+      expect(TestModel.type_name).to eql('geo')
+    end
+
+    it "allows determining the index type" do
+    end
+
+  end
+
+  context "instantiation" do
+
+    it "allows instantiating a model with no information" do
+      thing = TestModel.new
+      expect(thing).to be_a(Waistband::Model)
+      expect(thing.index).to be_a(Waistband::Index)
+      expect(thing.type_name).to eql('geo')
+      expect(thing.work_area).to be_nil
+
+      thing.work_area = {
+        type: 'polygon',
+        coordinates: [[
+          [-122.41997,37.79744]
+        ]]
+      }
+
+      expect(thing.work_area).to eql({
+        type: 'polygon',
+        coordinates: [[
+          [-122.41997,37.79744]
+        ]]
+      })
+    end
+
+    it "defaults fields" do
+      thing = TestModel.new
+      expect(thing.important).to eql(true)
+      expect(thing.shenanigans).to eql(1)
+    end
+
+  end
+
+  context "validations" do
+  end
+
+  context "callbacks" do
+  end
+
+end
