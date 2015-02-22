c215a5f7f887ba5782cfc9b40f1ca78f7e18aa78
David Jairala <davidjairala@gmail.com>
2015-02-21 22:34:53 -0800

    persistence and finding

--- lib/waistband/model.rb
+++ lib/waistband/model.rb
@@ -1,11 +1,14 @@
+require 'digest/md5'
 require 'active_support/core_ext/object/blank'
 require 'active_model'
 
 module Waistband
   class Model
 
+    extend ::ActiveModel::Callbacks
 
     include ::ActiveModel::Validations
+    include ::ActiveModel::Validations::Callbacks
 
 
     #################
@@ -34,16 +37,30 @@ module Waistband
         @column_defaults.merge!(defaults)
       end
 
+      def find(id)
+        found_attributes = index.find(id)
+
+        unless found_attributes
+          raise ::Waistband::Errors::Model::NotFound.new("Couldn't find #{self} with 'id'=#{id}")
+        end
+
+        new(found_attributes.merge(id: id))
+      end
+
     end
 
 
+    define_model_callbacks :save, :create, :destroy, :commit, :update
+
     delegate :index, :type_name, :column_defaults, to: :'self.class'
-    
-    attr_accessor :attributes
+
+    attr_accessor :attributes, :persisted, :id
 
 
     def initialize(attributes = {})
-      self.attributes = (column_defaults || {}).merge(attributes.symbolize_keys)
+      attributes = attributes.symbolize_keys
+      self.id = attributes.delete(:id)
+      self.attributes = ((column_defaults || {}).merge(attributes)).symbolize_keys
     end
 
 
@@ -52,7 +69,7 @@ module Waistband
     #########
 
     def property_names
-      index.config['mappings'][type_name]['properties'].keys
+      index.config['mappings'][type_name]['properties'].keys.map(&:to_sym)
     end
 
     ##############
@@ -60,17 +77,20 @@ module Waistband
     ##############
 
     def method_missing(method_name, *args, &block)
-      method_name_without_eq = method_name.to_s.gsub('=', '')
+      method_name_wout_eq = method_name.to_s.gsub('=', '')
 
       if self.attributes.has_key?(method_name)
         # attribute in place not by mappings but by defaults
         return read_attribute(method_name)
-      elsif property_names.include?(method_name.to_s)
+      elsif property_names.include?(method_name)
         # attribute reader (model.attribute_name)
         return read_attribute(method_name)
-      elsif property_names.include?(method_name_without_eq)
+      elsif property_names.include?(method_name_wout_eq)
         # attribute writer (model.attribute_name=)
-        write_attribute(method_name_without_eq, *args)
+        write_attribute(method_name_wout_eq, *args)
+      elsif method_name.to_s.match(/.+=$/)
+        # set unknown attribute
+        write_attribute(method_name_wout_eq, *args)
       else
         super
       end
@@ -83,6 +103,79 @@ module Waistband
     def write_attribute(key, *args)
       val = args.first
       self.attributes[key.to_sym] = val
+    end
+
+    ###############
+    # Persistence #
+    ###############
+
+    def save
+      result = false
+
+      return false unless self.valid?
+
+      run_callbacks :commit do
+        if self.new_record?
+          run_callbacks :create do
+            run_callbacks :save do
+              result = persist!
+            end
+          end
+        else
+          run_callbacks :update do
+            run_callbacks :save do
+              result = persist!
+            end
+          end
+        end
+      end
+
+      self.persisted = true if !self.persisted && result
+      result
+    end
+
+    def save!
+      result = save
+
+      unless result
+        raise ::Waistband::Errors::Model::UnableToSave.new("Save not successful: #{errors.full_messages}")
+      end
+
+      result
+    end
+
+    def persist!
+      set_id = false
+
+      unless self.id
+        self.id = digest
+        set_id = true
+      end
+
+      val = persist_to_es!
+
+      self.id = nil if !val && set_id
+
+      val
+    end
+
+    def persist_to_es!
+      # TODO: this should blow up if it fails
+      index.save(id, attributes)
+    end
+
+    def new_record?
+      !self.persisted?
+    end
+
+    def persisted?
+      !!persisted
+    end
+
+    def digest
+      keys = (property_names + column_defaults.keys).uniq
+      content = (keys - [:id]).map{|key| self.send(key) }.join('-')
+      Digest::MD5.hexdigest(content)
     end
 
 
--- spec/lib/model_spec.rb
+++ spec/lib/model_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Waistband::Model do
 
+
   class TestModel < Waistband::Model
 
     index_name :geo
@@ -11,7 +12,34 @@ describe Waistband::Model do
 
     validates :work_area, presence: true
 
+    before_validation :set_fake_attrs
+    before_create :set_more_fake_attrs
+
+    def set_fake_attrs
+      self.fake1 = 'uno'
+    end
+
+    def set_more_fake_attrs
+      self.fake2 = 'dos'
+    end
+
   end
+
+
+  let(:valid_work_area) do
+    {
+      'type' => 'polygon',
+      'coordinates' => [[
+        [-122.4119,37.78211],
+        [-122.39285,37.79649],
+        [-122.37997,37.78415],
+        [-122.39817,37.77248],
+        [-122.40932,37.77302],
+        [-122.4119,37.78211]
+      ]]
+    }
+  end
+
 
   context "index" do
 
@@ -35,19 +63,9 @@ describe Waistband::Model do
       expect(thing.type_name).to eql('geo')
       expect(thing.work_area).to be_nil
 
-      thing.work_area = {
-        type: 'polygon',
-        coordinates: [[
-          [-122.41997,37.79744]
-        ]]
-      }
+      thing.work_area = valid_work_area
 
-      expect(thing.work_area).to eql({
-        type: 'polygon',
-        coordinates: [[
-          [-122.41997,37.79744]
-        ]]
-      })
+      expect(thing.work_area).to eql(valid_work_area)
     end
 
     it "defaults fields" do
@@ -70,12 +88,62 @@ describe Waistband::Model do
   end
 
   context "callbacks" do
+
+    it "executes callbacks" do
+      thing = TestModel.new
+      thing.work_area = valid_work_area
+      expect(thing.valid?).to eql(true)
+      expect(thing.fake1).to eql('uno')
+
+      # not defined until after setting it
+      expect{ thing.fake2 }.to raise_error(NoMethodError)
+
+      expect(thing.save).to eql(true)
+
+      expect(thing.fake2).to eql('dos')
+    end
+
   end
 
-  context "persistance" do
+  context "persistence" do
+
+    it "persists an object" do
+      thing = TestModel.new
+      thing.work_area = valid_work_area
+      expect(thing.save).to eql(true)
+      expect(thing.id).to be_present
+      expect(thing.id).to be_a(String)
+    end
+
+    it "doesn't persist when not valid" do
+      thing = TestModel.new
+      expect(thing.valid?).to eql(false)
+      expect(thing.save).to eql(false)
+      expect(thing.id).to eql(nil)
+    end
+
   end
 
   context "reading" do
+
+    it "allows reading a previously saved object" do
+      thing = TestModel.new
+      thing.work_area = valid_work_area
+      expect(thing.save).to eql(true)
+      saved_id = thing.id
+      expect(saved_id).to be_present
+
+      found = TestModel.find(saved_id)
+      expect(found).to be_present
+      expect(found).to be_a(TestModel)
+      expect(found.id).to eql(saved_id)
+      expect(found.work_area).to eql(valid_work_area)
+    end
+
+    it "raises an error when we cannot find an id" do
+      expect{ TestModel.find('9191919191') }.to raise_error(::Waistband::Errors::Model::NotFound, "Couldn't find TestModel with 'id'=9191919191")
+    end
+
   end
 
   context "searching" do
